7abb2a9fdb2d419cbc492a0ab3ac9d15e7899a5d
ROGER YAU <codes@y-unit.com>
2023-08-16 12:41:53 -0700

    Implemented simple dark mode

---
+++ js/theme.js
@@ -0,0 +1,9 @@
+function darkMode() {
+    let element = document.body;
+    element.className = "dark-mode";
+}
+
+function lightMode() {
+    let element = document.body;
+    element.className = "light-mode";
+}
